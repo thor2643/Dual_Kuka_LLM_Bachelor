e77db39d56c997eec875d3a2ef14009d124988f2
Kasper Lauritsen <116707938+xdKazer@users.noreply.github.com>
2025-03-03 13:40:21 +0100

    Delete p5_ws/install directory

--- p5_ws/install/project_interfaces/local/lib/python3.10/dist-packages/project_interfaces/srv/__init__.py
+++
@@ -1,6 +0,0 @@
-from project_interfaces.srv._define_object_info import DefineObjectInfo  # noqa: F401
-from project_interfaces.srv._execute_move_command import ExecuteMoveCommand  # noqa: F401
-from project_interfaces.srv._get_current_pose import GetCurrentPose  # noqa: F401
-from project_interfaces.srv._get_object_info import GetObjectInfo  # noqa: F401
-from project_interfaces.srv._plan_move_command import PlanMoveCommand  # noqa: F401
-from project_interfaces.srv._prompt_janice import PromptJanice  # noqa: F401
--- p5_ws/install/robotiq_2f_85_interfaces/share/robotiq_2f_85_interfaces/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(robotiq_2f_85_interfaces_IDL_FILES "srv/Robotiq2F85GripperCommand.idl")
-set(robotiq_2f_85_interfaces_INTERFACE_FILES "srv/Robotiq2F85GripperCommand.srv;srv/Robotiq2F85GripperCommand_Request.msg;srv/Robotiq2F85GripperCommand_Response.msg")
