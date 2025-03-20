5ba32677deb62632db1c1506891b034931629e36
gustav-bay <gustavbay@live.dk>
2025-03-20 14:23:51 +0100

    Working communication between isaac and moveit (Moveit side)

--- p6_ws/src/lbr_fri/lbr_fri_ros2_stack/lbr_moveit_config/isaac_dual_arm_moveit_config/launch/demo.launch.py
+++ p6_ws/src/lbr_fri/lbr_fri_ros2_stack/lbr_moveit_config/isaac_dual_arm_moveit_config/launch/demo.launch.py
@@ -79,12 +79,5 @@ def generate_launch_description():
     
     
     
-    
-#from moveit_configs_utils import MoveItConfigsBuilder
-#from moveit_configs_utils.launches import generate_demo_launch
-#
-#
-#def generate_launch_description():
-#    moveit_config = MoveItConfigsBuilder("mock_dual_arm", package_name="mock_dual_arm_moveit_config").to_moveit_configs()
-#    return generate_demo_launch(moveit_config)
+
 
