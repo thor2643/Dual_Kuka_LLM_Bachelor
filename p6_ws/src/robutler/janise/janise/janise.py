38c559b0e9c3f9c12d525ab8f27827ffe687ee6c
Thor Iversen <thor951753@hotmail.com>
2025-05-15 00:23:04 +0200

    Real pipeline runs when errors are not encountered

--- p6_ws/src/robutler/janise/janise/janise.py
+++ p6_ws/src/robutler/janise/janise/janise.py
@@ -135,7 +135,6 @@ class LLMNode(Node):
         self.coordinates = { # Predefined poses for different locations
             'HOME_RIGHT_ARM': {'x': '0.1', 'y': '0.3', 'z': "0.3", 'roll': '0', 'pitch': '0', 'yaw': '0'},
             'HOME_LEFT_ARM': {'x': '0.9', 'y': '0.3', 'z': "0.3", 'roll': '0', 'pitch': '0', 'yaw': '0'},
-            'TAKE_IMAGE': {'x': '0.42', 'y': '0.83', 'z': '0.5', 'roll': '-3', 'pitch': '-43', 'yaw': '-83'},
         }
 
         self.sim_tool_list = {}
